b7fedcf0db6e9b7694d2bc689b3dcc90abe60480
ErezAmirav <erezamirav@gmail.com>
2021-12-13 19:30:21 +0200

    Loading time

--- app/src/main/java/com/example/omtiamt/Login.java
+++ app/src/main/java/com/example/omtiamt/Login.java
@@ -2,7 +2,11 @@ package com.example.omtiamt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+       /* new Timer().schedule(new TimerTask() {
+            @Override
+            public void run(){
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 5000);*/
     }
 }
--- app/src/main/java/com/example/omtiamt/MainActivity.java
+++ app/src/main/java/com/example/omtiamt/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.omtiamt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run(){
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        }, 4000);
+
     }
 }
