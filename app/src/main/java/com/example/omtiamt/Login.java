6dce7697ef57b4c682277732686bd74cee216b3d
erezamirav <erezamirav@gmail.com>
2021-12-20 15:47:18 +0200

    Added register fragment

--- app/src/main/java/com/example/omtiamt/Login.java
+++ app/src/main/java/com/example/omtiamt/Login.java
@@ -1,25 +1,42 @@
 package com.example.omtiamt;
 
+import static com.example.omtiamt.R.id.login_clickhere_id;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.omtiamt.Model.RegisterFragment;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Login extends AppCompatActivity {
 
+public class Login extends AppCompatActivity {
+    TextView Register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-       /* new Timer().schedule(new TimerTask() {
+        Register = findViewById(login_clickhere_id);
+        Register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run(){
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.login, new RegisterFragment()).commit();
             }
-        }, 5000);*/
+        });
+
+
+
+
+
     }
 }
