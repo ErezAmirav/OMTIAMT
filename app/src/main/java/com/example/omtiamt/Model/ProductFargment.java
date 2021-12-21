81f00e8ebf5639bef2ae30cb3f11368e0c98e20a
erezamirav <erezamirav@gmail.com>
2021-12-21 15:34:58 +0200

    Added navigation bar

---
+++ app/src/main/java/com/example/omtiamt/Model/ProductFargment.java
@@ -0,0 +1,66 @@
+package com.example.omtiamt.Model;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.omtiamt.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProductFargment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProductFargment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ProductFargment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProductFargment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProductFargment newInstance(String param1, String param2) {
+        ProductFargment fragment = new ProductFargment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product, container, false);
+    }
+}
--- app/src/main/java/com/example/omtiamt/Model/RegisterFragment.java
+++ app/src/main/java/com/example/omtiamt/Model/RegisterFragment.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import com.example.omtiamt.R;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,16 +20,16 @@ import com.google.firebase.database.FirebaseDatabase;
  */
 public class RegisterFragment extends Fragment {
     ImageButton btn;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_register, container, false);
         btn = (ImageButton) view.findViewById(R.id.signup_register_btn_id);
         btn.setOnClickListener((v -> {
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef = database.getReference("message");
-            myRef.setValue("Hello, World");
             Navigation.findNavController(v).navigate(R.id.action_registerFragment2_to_login);
             //Navigation.findNavController(v).navigateUp();
         }));
