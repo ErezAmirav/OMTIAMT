7547c31ea71117d0d5380c977325e354451d500f
ErezAmirav <erezamirav@gmail.com>
2022-01-04 19:01:37 +0200

    Register fragment fix

--- app/src/main/java/com/example/omtiamt/Model/RegisterFragment.java
+++ app/src/main/java/com/example/omtiamt/Model/RegisterFragment.java
@@ -1,13 +1,10 @@
 package com.example.omtiamt.Model;
 
-import android.content.Intent;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
+
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +12,10 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import com.example.omtiamt.Login;
 import com.example.omtiamt.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -36,84 +28,68 @@ public class RegisterFragment extends Fragment {
     View view;
     private FirebaseAuth mAuth;
     FirebaseUser currentUser;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         mAuth = FirebaseAuth.getInstance();
-
         view = inflater.inflate(R.layout.fragment_register, container, false);
         btnSaveUser = view.findViewById(R.id.signup_register_btn_id);
         inputPassword = view.findViewById(R.id.password_register_id);
         inputEmail = view.findViewById(R.id.email_register_id);
         inputConfirmPassword = view.findViewById(R.id.confirmpassword_register_id);
-        btnSaveUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(inputEmail.getText().toString())){
-                    inputEmail.setError("Email Field Cannot Be Empty");
-                    inputEmail.requestFocus();
-                }
-                else if (TextUtils.isEmpty(inputPassword.getText().toString()))
-                {
-                    inputPassword.setError("Password Field Cannot Be Empty");
-                    inputPassword.requestFocus();
-                }
-                else if (TextUtils.isEmpty(inputConfirmPassword.getText().toString()))
-                {
-                        inputConfirmPassword.setError("Confirm Password Field Cannot Be Empty");
-                        inputConfirmPassword.requestFocus();
-                }
-                else if (isValid(inputEmail.getText().toString()) == false)
-                {
-                    inputEmail.setError("Email not Legal");
-                    inputEmail.requestFocus();
-                }
-                else if (inputPassword.getText().toString().equals(inputConfirmPassword.getText().toString()))
-                {
-                        model.instance.registerNewUser(inputEmail.getText().toString(), inputPassword.getText().toString());
-                }
-                else
-                {
-                    Toast.makeText(RegisterFragment.this.getContext(), "The Password are not equals", Toast.LENGTH_LONG).show();
-                }
+        btnSaveUser.setOnClickListener(v -> {
 
+            // Checks if Email field is not empty
+            if (TextUtils.isEmpty(inputEmail.getText().toString())) {
+                inputEmail.setError("Email Field Cannot Be Empty");
+                inputEmail.requestFocus();
             }
+            // Checks if Password field is not empty
+            else if (TextUtils.isEmpty(inputPassword.getText().toString())) {
+                inputPassword.setError("Password Field Cannot Be Empty");
+                inputPassword.requestFocus();
+            }
+            // Checks if Confirm Password field is not empty
+            else if (TextUtils.isEmpty(inputConfirmPassword.getText().toString())) {
+                inputConfirmPassword.setError("Confirm Password Field Cannot Be Empty");
+                inputConfirmPassword.requestFocus();
+            }
+            // Checks for Password field length
+            else if (inputPassword.getText().toString().length() < 6) {
+                inputPassword.setError("Password must be 6 chars or longer");
+                inputPassword.requestFocus();
+            }
+            // Checks if Email field is valid
+            else if (!isValid(inputEmail.getText().toString())) {
+                inputEmail.setError("Email is not Legal");
+                inputEmail.requestFocus();
+            }
+            // Checks if Password and Confirm Password fields match
+            else if (inputPassword.getText().toString().equals(inputConfirmPassword.getText().toString()))
+                model.instance.registerNewUser(inputEmail.getText().toString(), inputPassword.getText().toString());
+            else
+                Toast.makeText(RegisterFragment.this.getContext(), "The Passwords don't match", Toast.LENGTH_LONG).show();
         });
         return view;
     }
 
     public void onStart() {
         super.onStart();
-        //FirebaseAuth.getInstance().signOut();
         currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-                Toast.makeText(RegisterFragment.this.getContext(), "Current User Online", Toast.LENGTH_LONG).show();
-           // Navigation.findNavController(view).navigate(R.id.action_registerFragment2_to_login2);
-
+            Toast.makeText(RegisterFragment.this.getContext(), "Current User Online", Toast.LENGTH_LONG).show();
         } else
             Toast.makeText(RegisterFragment.this.getContext(), "User Offline", Toast.LENGTH_LONG).show();
     }
 
-
-
-    public void logout_btn2(View view){
+    public void logout_btn2(View view) {
         FirebaseAuth.getInstance().signOut();
         onStart();
     }
+
     static boolean isValid(String email) {
         String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
         return email.matches(regex);
     }
 }
-
-
-
-    /*
-    String name = inputusername.getText().toString();
-        String password = inputpassword.getText().toString();
-        String email = inputemail.getText().toString();
-        Users user = new Users(name, password, email);
-        model.instance.addUser(user, () -> {
-            Navigation.findNavController(inputusername).navigateUp();
-     */
