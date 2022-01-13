763ef06d0c4fc178a2bfd011840ef27c9ec1e8ee
idantalmor <75263070+idantalmor@users.noreply.github.com>
2022-01-13 14:22:03 +0200

    Add spinner at create Product

--- app/src/main/java/com/example/omtiamt/Model/NewProductFragment.java
+++ app/src/main/java/com/example/omtiamt/Model/NewProductFragment.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.omtiamt.R;
 
@@ -16,9 +18,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 
-public class NewProductFragment extends Fragment {
+public class NewProductFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     View view;
     Spinner catList;
+    TextView TextViewcatChoose;
 
 
     @Override
@@ -33,16 +36,29 @@ public class NewProductFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_new_product, container, false);
         catList = view.findViewById(R.id.choose_category_id);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(NewProductFragment.this.getContext()
-                ,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.names));
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        TextViewcatChoose = view.findViewById(R.id.Textview_catChoose);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),R.array.names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         catList.setAdapter(adapter);
+        catList.setOnItemSelectedListener(this);
         return view;
     }
 
     public void getCategoryName(){
         List<String> list = new LinkedList<String>();
         list = model.instance.getAllCategoriesName();
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String cateChoose = parent.getItemAtPosition(position).toString();
+        TextViewcatChoose.setText(cateChoose);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
