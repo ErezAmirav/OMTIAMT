1fd0734c799296ac8b15a92edca8b37134f255c1
ErezAmirav <erezamirav@gmail.com>
2022-01-13 14:02:07 +0200

    Added dropdown menu (spinner) to new product page

--- app/src/main/java/com/example/omtiamt/Model/NewProductFragment.java
+++ app/src/main/java/com/example/omtiamt/Model/NewProductFragment.java
@@ -7,64 +7,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.omtiamt.R;
 
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NewProductFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NewProductFragment extends Fragment {
+    View view;
+    Spinner catList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NewProductFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewProductFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewProductFragment newInstance(String param1, String param2) {
-        NewProductFragment fragment = new NewProductFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_product, container, false);
+
+        view = inflater.inflate(R.layout.fragment_new_product, container, false);
+        catList = view.findViewById(R.id.choose_category_id);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(NewProductFragment.this.getContext()
+                ,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.names));
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        catList.setAdapter(adapter);
+        return view;
     }
 
     public void getCategoryName(){
