39c9fafee5622c716719864a904b744f270dad39
ErezAmirav <erezamirav@gmail.com>
2022-01-20 20:56:47 +0200

    Added change profile picture / change username menu button

--- app/src/main/java/com/example/omtiamt/Model/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/omtiamt/Model/Fragments/ProfileFragment.java
@@ -1,16 +1,29 @@
 package com.example.omtiamt.Model.Fragments;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+
+import android.provider.MediaStore;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -21,6 +34,8 @@ import com.example.omtiamt.Model.Recylers.MyProductsListFragment;
 import com.example.omtiamt.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.io.InputStream;
 
 
 public class ProfileFragment extends Fragment {
@@ -33,9 +48,20 @@ public class ProfileFragment extends Fragment {
     ImageButton settingsMenu;
     PopupMenu popupMenu;
     AlertDialog.Builder alert;
-    Button savedProductsBtn;
+    ImageView profilePicture;
     View fragmentMyProduct;
     ProgressBar progressBar;
+    AlertDialog.Builder newPictureAlert;
+    AlertDialog.Builder newUserName;
+    TextView profileName;
+    String imageUrl;
+    Bitmap imageBitmap;
+    private String myText;
+
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_OPEN_GALLERY = 2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,12 +74,13 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         BaseActivity.showTabBar();
         view = inflater.inflate(R.layout.fragment_profile, container, false);
+        profileName = view.findViewById(R.id.profile_name_id);
         progressBar = view.findViewById(R.id.product_by_category_pb);
         progressBar.setVisibility(View.GONE);
+        profilePicture = view.findViewById(R.id.profile_picture_iv);
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        savedProductsBtn = view.findViewById(R.id.profile_saved_products_btn);
         // Show Current User Email
         email = view.findViewById(R.id.profile_email_id);
         userEmail = " " + mAuth.getCurrentUser().getEmail();
@@ -63,9 +90,6 @@ public class ProfileFragment extends Fragment {
         MyProductsListFragment fragment = (MyProductsListFragment) getChildFragmentManager().findFragmentById(R.id.productByMe);
         fragment.SetMyName(userEmail2);
         fragmentMyProduct = view.findViewById(R.id.productByMe);
-        savedProductsBtn.setOnClickListener(v -> {
-            Navigation.findNavController(view).navigate(ProfileFragmentDirections.actionProfileFragmentToProductIWantFragment2(userEmail2));
-        });
 
         // Settings Popup Menu
         settingsMenu = view.findViewById(R.id.profile_settings_id);
@@ -89,9 +113,8 @@ public class ProfileFragment extends Fragment {
                                 Navigation.findNavController(view).navigate(R.id.action_profileFragment_to_registerFragment);
                             });
                         });
-                        alert.setNegativeButton("No", (dialog, which) -> {
-                            Toast.makeText(ProfileFragment.this.getContext(), "Aborted", Toast.LENGTH_LONG).show();
-                        });
+                        alert.setNegativeButton("No", (dialog, which) ->
+                                Toast.makeText(ProfileFragment.this.getContext(), "Aborted", Toast.LENGTH_LONG).show());
                         alert.create().show();
                         return true;
 
@@ -104,6 +127,44 @@ public class ProfileFragment extends Fragment {
                         startActivity(intent);
                     }
                     return true;
+                    case R.id.item_change_profile_picture:
+                        newPictureAlert = new AlertDialog.Builder(ProfileFragment.this.getContext());
+                        newPictureAlert.setTitle("Change Profile Picture");
+                        newPictureAlert.setMessage("Take Picture / Open Gallery");
+
+                        newPictureAlert.setPositiveButton("Open Camera", (dialog, which) -> {
+                            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                        });
+
+                        newPictureAlert.setNegativeButton("Open Gallery", (dialog, which) -> {
+                            Intent openGalleryIntent = new Intent(Intent.ACTION_PICK);
+                            openGalleryIntent.setType("image/*");
+                            startActivityForResult(openGalleryIntent, REQUEST_OPEN_GALLERY);
+                        });
+                        newPictureAlert.create().show();
+                        return true;
+
+                    case R.id.item_my_saved_products:
+                        Navigation.findNavController(view).navigate(ProfileFragmentDirections.actionProfileFragmentToProductIWantFragment2(userEmail2));
+                        return true;
+
+                    case R.id.item_change_profile_name:
+                        newUserName = new AlertDialog.Builder(ProfileFragment.this.getContext());
+                        newUserName.setTitle("Full Details");
+
+                        final EditText fullDetails = new EditText(ProfileFragment.this.getContext());
+                        fullDetails.setInputType(InputType.TYPE_CLASS_TEXT);
+                        newUserName.setView(fullDetails);
+
+                        newUserName.setPositiveButton("Confirm", (dialog, which) -> {
+                            myText = fullDetails.getText().toString();
+                            profileName.setText(myText);
+                        });
+                        newUserName.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+                        newUserName.show();
+                        return true;
+
                     default:
                         return false;
                 }
@@ -111,5 +172,30 @@ public class ProfileFragment extends Fragment {
             popupMenu.show();
         });
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                imageBitmap = (Bitmap) extras.get("data");
+                profilePicture.setImageBitmap(imageBitmap);
+            }
+        }
+        if (requestCode == REQUEST_OPEN_GALLERY) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    final Uri imageUri = data.getData();
+                    final InputStream imgStream = getContext().getContentResolver().openInputStream(imageUri);
+                    imageBitmap = BitmapFactory.decodeStream(imgStream);
+                    profilePicture.setImageBitmap(imageBitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(ProfileFragment.this.getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
